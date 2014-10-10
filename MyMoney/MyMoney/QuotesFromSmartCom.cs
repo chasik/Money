7ff823935bc570716aceaa69450c03e7d940ab2c
Ivan Chasov <chasow@yandex.ru>
2014-10-10 14:48:42 +0400

    запасной торговый сервер

--- MyMoney/MyMoney/QuotesFromSmartCom.cs
+++ MyMoney/MyMoney/QuotesFromSmartCom.cs
@@ -70,10 +70,17 @@ namespace MyMoney
             scom = new SmartCOM3Lib.StServerClass();
             scom.ConfigureClient("logLevel=5;CalcPlannedPos=no;logFilePath=D:");
             scom.ConfigureServer("logLevel=5;pingTimeOut=5;logFilePath=D:");
-            scom.connect("mx.ittrade.ru", 8443, login, password); workPortfolioName = "BP12800-RF-01";
+            //scom.connect("mx.ittrade.ru", 8443, login, password); workPortfolioName = "BP12800-RF-01";
+            scom.connect("st1.ittrade.ru", 8090, login, password); workPortfolioName = "BP12800-RF-01";
             //scom.connect("mxdemo.ittrade.ru", 8443, "C9GAAL6V", "VKTFP3");  workPortfolioName = "ST59164-RF-01"; // тестовый доступ
             workSymbol = "RTS-12.14_FT";
             scom.Connected += scom_Connected;
+            scom.Disconnected += scom_Disconnected;
+        }
+
+        void scom_Disconnected(string reason)
+        {
+            MessageBox.Show("Причина отключения: " + reason);
         }
 
         void scom_Connected()
